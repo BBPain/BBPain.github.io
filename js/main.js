ae9166dddec89088963fed3355c92a2356f1f870
grzegorzbors <gbors@o2.pl>
2018-04-10 20:40:43 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    // slide toggle nav
+    $("#hidden-menu").on('click', function (e) {
+        e.preventDefault();
+        $('#menu-list').slideToggle();
+    });
+
+    // remove inline attribute display:none created by toggle func
+    $(window).resize(function () {
+        let width = $(window).width();
+        if (width > 576 && $('#menu-list').is(':hidden')) {
+            $('#menu-list').removeAttr('style');
+        }
+    });
+});
